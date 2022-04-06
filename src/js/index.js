8306ab22d0b8d2c4f04dd566353a91703293504d
altumus <jojojopa@icloud.com>
2022-04-06 22:23:05 +0300

    new blocks

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,20 @@ import * as Vue from 'vue'
 
 import mainblock from '../vue/main-block.vue';
 import secondblock from '../vue/second-block.vue';
+import thirdblock from '../vue/third-block.vue';
+
+
 
 import '../css/index.less'
 import '../css/reset.css'
+import '../css/slider.less'
+
+
+
+import '../../node_modules/@glidejs/glide/dist/css/glide.core.min.css'
+import '../../node_modules/@glidejs/glide/dist/css/glide.theme.min.css'
+
 
 Vue.createApp(mainblock).mount('#main-container')
 Vue.createApp(secondblock).mount('#second-container')
+Vue.createApp(thirdblock).mount('#third-container')
---
+++ src/vue/third-block.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="slider">
+        </div>
+    </div>
+    <div class="control">
+        <div class="arrow" id="left" onclick=slide()><img src="../pics/arrow_active.svg" style="transform: rotate(180deg)"></div>
+        <div class="bullet"></div>
+        <div class="bullet"></div>
+        <div class="bullet"></div>
+        <div class="bullet"></div>
+        <div class="bullet"></div>
+        <div class="arrow" id="right" onclick=slide()><img src="../pics/arrow_active.svg"></div>
+    </div>
+</template>
+
+<script>
+    function slide(some){
+        console.log(some);
+    }
+</script>
